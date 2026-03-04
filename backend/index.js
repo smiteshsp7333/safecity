1cbb76b78f8ba6b72df7233bbc8064b16b1f7acc
SmiteshP7333 <157452848+SmiteshP7333@users.noreply.github.com>
2026-03-04 18:03:47 +0530

    fix port for render deployment

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ io.on('connection', (socket) => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected ✅');
-    server.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT || 5000, () => {
       console.log(`Server running on port ${process.env.PORT} 🚀`);
     });
   })
